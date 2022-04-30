c1b94f5c88227f4a9e36c5fcde052defdea3ac67
ChristianOConnor <warpanomaly@gmail.com>
2022-04-30 03:05:10 -0400

    add google-auth-library function

--- components/Middle.tsx
+++ components/Middle.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import {GoogleAuth} from 'google-auth-library';
 
 class Middle extends React.Component {
-    handleClick() {
-        console.log('this is:', this);
-      }
-    // This syntax ensures `this` is bound within handleClick.  // Warning: this is *experimental* syntax.  handleClick = () => {    console.log('this is:', this);  }
-
-    async fetchNonce() {
-        const response = await fetch("http://localhost:<PORT NUMBER HERE>/path/to/endpoint")
-        console.log(response.status)
-        const data = await response.json()
-        console.log(data)
-    }
-
-    render() {
-      return (
-        <div className="col-md-12 text-center">
-            <Button variant='primary' onClick={this.fetchNonce}>
-                Click me
-            </Button>
-        </div>
-      );
-    }
+  handleClick() {
+    console.log('this is:', this);
   }
+
+  async fetchNonce() {
+    // Define your URL, here with Cloud Run but the security is exactly the same with Cloud Functions (same underlying infrastructure)
+    const url = process.env.FUNCTION_URL
+    
+    //Example with the key file, not recommended on GCP environment.
+    const auth = new GoogleAuth({keyFilename: process.env.KEYSTORE_PATH})
+
+    //Create your client with an Identity token.
+    const client = await auth.getIdTokenClient(url);
+    const res = await client.request({url});
+    console.log(res.data);
+  }
+
+  render() {
+    return (
+      <div className="col-md-12 text-center">
+        <Button variant='primary' onClick={this.fetchNonce}>
+          Click me
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default Middle;
